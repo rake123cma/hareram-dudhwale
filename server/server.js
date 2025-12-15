483b7e2b4c2d6451f2273496ce4f2f5475e66d7d
Rakesh Kumar <rake123cma@gmail.com>
2025-12-15 09:11:32 +0530

    Fix CORS issue for production deployment

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
 
   // CORS configuration
-  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN || 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
